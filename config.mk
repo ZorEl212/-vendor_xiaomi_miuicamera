d585e8d085d1cc284f6b87fdffe00d41cfc493e4
ZorEl212 <yabsirad212@gmail.com>
2024-10-16 15:00:33 +0300

    Adapt ANXCamera for Ginkgo to work with AOSP source.  Original module: [1]
    [1] https://github.com/h1toru/ANXCameraT
    
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- config.mk
+++ config.mk
@@ -1,43 +1,28 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/miuicamera
 
+
+LOCAL_PATH := vendor/xiaomi/miuicamera
+
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/priv-app/MiuiCamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/priv-app/MiuiExtraPhoto/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiExtraPhoto/lib) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/etc,$(TARGET_COPY_OUT_VENDOR)/etc) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/camera,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/camera)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/system/priv-app/ANXCamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib) \
+
+PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/features/ginkgo.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/device_features/ginkgo.xml \
+    $(LOCAL_PATH)/features/cheatcodes/ginkgo.json:$(TARGET_COPY_OUT_SYSTEM)/etc/ANXCamera/cheatcodes/cheatcodes.json
 
 PRODUCT_PACKAGES += \
-    MiuiCamera \
-    MiuiExtraPhoto \
-    MiuiScanner
+    ANXCamera 
 
 # Props
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.lens.oem_camera_package=com.android.camera
+PRODUCT_PRODUCT_OVERRIDES += \
+    persist.camera.eis.enabled=1 \
+    ro.camera.sound.forced=0 \
+    vendor.camera.aux.packagelist=com.android.camera,app.grapheneos.camera \
+    vendor.camera.aux.packageblacklist=org.telegram.messenger,com.discord \
+    persist.vendor.camera.privapp.list=com.android.camera,app.grapheneos.camera
 
 # Sepolicy
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     vendor/xiaomi/miuicamera/sepolicy/private
-
-ifneq ($(findstring davinci, $($TARGET_PRODUCT)),)
-    $(call inherit-product, vendor/xiaomi/miuicamera/configs/davinci.mk)
-endif
-
-ifneq ($(findstring raphael, $($TARGET_PRODUCT)),)
-    $(call inherit-product, vendor/xiaomi/miuicamera/configs/raphael.mk)
-endif
-
-ifneq ($(findstring cepheus, $($TARGET_PRODUCT)),)
-    $(call inherit-product, vendor/xiaomi/miuicamera/configs/cepheus.mk)
-endif
-
-ifneq ($(findstring toco, $($TARGET_PRODUCT)),)
-    $(call inherit-product, vendor/xiaomi/miuicamera/configs/toco.mk)
-endif
-
-ifneq ($(findstring fog, $($TARGET_PRODUCT)),)
-    $(call inherit-product, vendor/xiaomi/miuicamera/configs/fog.mk)
-endif
